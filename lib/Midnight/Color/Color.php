ff7f9ad05f7e3c12129ee297eda60cecd628f151
Rudolph Gottesheim <r.gottesheim@loot.at>
2013-08-15 19:26:23 +0200

    Initial commit

---
+++ lib/Midnight/Color/Color.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Midnight\Color;
+
+/**
+ * Class Image
+ * @package Midnight\Color
+ */
+class Color
+{
+    /**
+     * @var int
+     */
+    private $r;
+    /**
+     * @var int
+     */
+    private $g;
+    /**
+     * @var int
+     */
+    private $b;
+
+    function __construct($r, $g, $b)
+    {
+        $this->setR($r);
+        $this->setG($g);
+        $this->setB($b);
+    }
+
+    /**
+     * @return int
+     */
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    /**
+     * @param int $b
+     */
+    public function setB($b)
+    {
+        $this->b = $b;
+    }
+
+    /**
+     * @return int
+     */
+    public function getG()
+    {
+        return $this->g;
+    }
+
+    /**
+     * @param int $g
+     */
+    public function setG($g)
+    {
+        $this->g = $g;
+    }
+
+    /**
+     * @return int
+     */
+    public function getR()
+    {
+        return $this->r;
+    }
+
+    /**
+     * @param int $r
+     */
+    public function setR($r)
+    {
+        $this->r = $r;
+    }
+}
